21c8bcbefc6238735854151fad12e4a9de92f926
kji-dec <dec22sag@gmail.com>
2023-02-17 13:35:31 +0900

    feat: Create detail model
    related to #2

---
+++ ubalpha/detail/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class Detail(models.Model):
+    member = models.ForeignKey('member.Member', on_delete=models.CASCADE)
+    planet = models.ForeignKey('planet.Planet', on_delete=models.CASCADE)
+    character = models.ForeignKey('character.Character', on_delete=models.CASCADE)
+    status = models.CharField(max_length=128, default='unused',
+        choices=(
+            ('unused', 'unused'),
+            ('used', 'used'),
+        ),
+    )
+    point = models.IntegerField(default=0)
+
+    class Meta:
+        db_table = 'detail'
