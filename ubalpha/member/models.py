0419fca60f7ea1cb493a76bc56c89e328b53eaba
kji-dec <dec22sag@gmail.com>
2023-02-17 12:59:04 +0900

    feat: Create member model
    related to #2

--- ubalpha/member/models.py
+++ ubalpha/member/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Member(models.Model):
+    spacename = models.CharField(max_length=256, null=True)
+    point = models.IntegerField(default=0)
+
+    class Meta:
+        db_table = 'alpha_member'
