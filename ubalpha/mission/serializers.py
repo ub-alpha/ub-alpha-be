5f22a491471714babd0650a7b16b59f01c423b58
kji-dec <dec22sag@gmail.com>
2023-02-21 08:02:29 +0900

    feat: Add status in mission response
    added status to show what user did in mission tab
    
    resolves #13

--- ubalpha/mission/serializers.py
+++ ubalpha/mission/serializers.py
@@ -2,7 +2,21 @@ from rest_framework import serializers
 
 from .models import Mission, Log
 
+import datetime
+
 class MissionSerializer(serializers.ModelSerializer):
+    status = serializers.SerializerMethodField()
+
+    def get_status(self, obj):
+        log = Log.objects.filter(member=self.context['request'].user, mission=obj.id)
+        if len(log) == 0:
+            return 'notready'
+        if obj.category == 'welcome':
+            return log[0].status
+        if log[len(log)-1].created_at != datetime.datetime.now().date():
+            return 'notready'
+        return 'ready'
+
     class Meta:
         model = Mission
         fields = '__all__'
