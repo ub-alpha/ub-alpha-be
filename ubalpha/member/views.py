4cfcaf550a2522ea1994a14b7ed3d079abc5fecb
kji-dec <dec22sag@gmail.com>
2023-02-20 13:15:19 +0900

    fix: Add save function

--- ubalpha/member/views.py
+++ ubalpha/member/views.py
@@ -2,7 +2,7 @@ from rest_framework import mixins, generics, status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from .models import Member
+
 from .serializers import MemberSerializer
 
 class MemberRegisterView(
@@ -38,6 +38,7 @@ class MemberDetailView(
         
         member = request.user
         member.spacename = spacename
+        member.save()
 
         return Response({
             "spacename": member.spacename
