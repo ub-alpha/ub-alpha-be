825ddd3b6d71cfe41e87435780f72a1cc64a455d
kji-dec <dec22sag@gmail.com>
2023-02-20 11:41:06 +0900

    feat: Add check permission about member
    using permission class, added permission check logic
    
    resolves #8

--- ubalpha/member/urls.py
+++ ubalpha/member/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.MemberView.as_view()),
-    path('/<int:pk>', views.MemberDetailView.as_view()),
+    path('', views.MemberRegisterView.as_view()),
+    path('/spacename', views.MemberDetailView.as_view()),
 ]
--- ubalpha/member/views.py
+++ ubalpha/member/views.py
@@ -1,8 +1,11 @@
-from rest_framework import mixins, generics
+from rest_framework import mixins, generics, status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 from .models import Member
 from .serializers import MemberSerializer
 
-class MemberView(
+class MemberRegisterView(
     mixins.CreateModelMixin,
     generics.GenericAPIView,
 ):
@@ -12,14 +15,30 @@ class MemberView(
         return self.create(request, args, kwargs)
 
 class MemberDetailView(
-    mixins.RetrieveModelMixin,
-    mixins.UpdateModelMixin,
-    generics.GenericAPIView,
+    APIView
 ):
     serializer_class = MemberSerializer
-
-    def get_queryset(self):
-        return Member.objects.all().order_by('id')
+    permission_classes = [
+        IsAuthenticated,
+    ]
     
     def get(self, request, *args, **kwargs):
-        return self.retrieve(request, args, kwargs)
+        member = request.user
+        return Response({
+            "spacename": member.spacename,
+            "point": member.point,
+        }, status=status.HTTP_200_OK)
+    
+    def put(self, request, *args, **kwargs):
+        spacename = request.data.get('spacename')
+        if len(spacename) > 10 or spacename == "":
+            return Response({
+                "detail": "space name should not be null and length of space name should be less than 11"
+            }, status=status.HTTP_400_BAD_REQUEST)
+        
+        member = request.user
+        member.spacename = spacename
+
+        return Response({
+            "spacename": member.spacename
+        }, status=status.HTTP_200_OK)
